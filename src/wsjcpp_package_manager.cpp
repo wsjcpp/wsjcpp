073109c46914602ac3415840d0d502e2070bd2fd
Evgenii Sopov <mrseakg@gmail.com>
2020-04-10 16:58:08 +0700

    Implemented pack rsources binary and text

--- src/wsjcpp_package_manager.cpp
+++ src/wsjcpp_package_manager.cpp
@@ -401,7 +401,7 @@ WsjcppPackageManager::WsjcppPackageManager(const std::string &sDir) {
     m_sHttpPrefix = "http://"; // from some http://
     m_sHttpsPrefix = "https://";
     m_sYamlFilename = "wsjcpp.yml";
-    m_sWsjcppCurrentVersion = "v0.1.2";
+    m_sWsjcppCurrentVersion = std::string(WSJCPP_VERSION);
     m_sWsjcppVersion = m_sWsjcppCurrentVersion;
     m_bHolded = false;
     m_sIssues = "none";
